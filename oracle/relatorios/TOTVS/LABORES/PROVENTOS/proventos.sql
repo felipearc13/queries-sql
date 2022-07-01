d50c6795c23ea15eb84491d77db354a20cff547f
felipearc13 <felipesilva1302@gmail.com>
2022-07-01 13:59:56 -0300

    new

--- oracle/relatorios/TOTVS/LABORES/PROVENTOS/proventos.sql
+++ oracle/relatorios/TOTVS/LABORES/PROVENTOS/proventos.sql
@@ -1,10 +1,24 @@
-SELECT *
+SELECT
+    pffinanc.codevento,
+    pevento.descricao,
+    SUM(pffinanc.valor)
 FROM
     u_cfl8u4_rm.pffinanc
     LEFT JOIN u_cfl8u4_rm.pevento ON pffinanc.codevento = pevento.codigo
+    LEFT JOIN u_cfl8u4_rm.pfunc ON pffinanc.chapa = pfunc.chapa
+    LEFT JOIN u_cfl8u4_rm.psecao ON pfunc.codsecao = psecao.codigo
 WHERE
-        chapa = '012329'
-    AND nroperiodo = 3
-    AND anocomp = 2022
-    AND mescomp = 4
+    /*    chapa = '012329'
+    AND nroperiodo = 3 
+    AND */
+    nroperiodo BETWEEN :periodo_1 AND :periodo_2
+    AND anocomp = 2022 /*ano*/
+    AND mescomp = 4 /*mes*/
+    AND pfunc.codsecao LIKE :secao || '%'
+    AND pffinanc.valor <> 0
     AND provdescbase = 'P'
+GROUP BY
+    pffinanc.codevento,
+    pevento.descricao
+ORDER BY
+    pffinanc.codevento
